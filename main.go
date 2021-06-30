a1470d9c970d1b6c4d711ac138e9498c07867f5f
Erik Fastermann <erik@fastermann.de>
2021-06-30 18:02:49 +0200

    first run :)

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
 	"os"
+
+	"github.com/erikfastermann/quinn/parser"
+	"github.com/erikfastermann/quinn/run"
 )
 
 func main() {
@@ -14,26 +16,31 @@ func main() {
 		return
 	}
 
-	l := NewLexer(bytes.NewReader(b))
-	for {
-		t, err := l.Next()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Fprintln(os.Stderr, err)
-		}
-		if _, ok := t.(EndOfLine); ok {
-			fmt.Println()
-		} else {
-			fmt.Printf("%s ", t)
-		}
-	}
-	fmt.Println()
+	// l := NewLexer(bytes.NewReader(b))
+	// for {
+	// 	t, err := l.Next()
+	// 	if err != nil {
+	// 		if err == io.EOF {
+	// 			break
+	// 		}
+	// 		fmt.Fprintln(os.Stderr, err)
+	// 	}
+	// 	if _, ok := t.(EndOfLine); ok {
+	// 		fmt.Println()
+	// 	} else {
+	// 		fmt.Printf("%s ", t)
+	// 	}
+	// }
+	// fmt.Println()
 
-	block, err := Parse(NewLexer(bytes.NewReader(b)))
+	block, err := parser.Parse(parser.NewLexer(bytes.NewReader(b)))
 	fmt.Println(block.String())
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	if err := run.Run(block); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		return
 	}
--- run/run.go
+++ run/run.go
@@ -8,13 +8,44 @@ import (
 	"github.com/erikfastermann/quinn/parser"
 )
 
+// TODO:
+//	better printing for vals
+//	define function with args
+//	Number/parser.Number as pointer
+
 const internal = "internal error"
 
 type Value interface {
 	value()
 }
 
-func valueFromGroup(parser.Group) (Value, ok) {
+type namedValue struct {
+	name  *name
+	value Value // nil == no underlying value
+}
+
+func (v namedValue) get() (Value, error) {
+	if v.value == nil {
+		// TODO: better error message with possible name
+		return nil, errors.New("no value")
+	}
+	return v.value, nil
+}
+
+func (v namedValue) onlyName() (string, error) {
+	if v.name == nil || v.value != nil {
+		if v.name == nil {
+			return "", fmt.Errorf("value %#v is unnamed", v.value)
+		} else {
+			return "", fmt.Errorf("%s has underlying value %#v", v.name.n, v.value)
+		}
+	}
+	return v.name.n, nil
+}
+
+type name struct {
+	n    string
+	next *name
 }
 
 type Unit struct{}
@@ -29,84 +60,243 @@ type String string
 
 func (String) value() {}
 
-type Number big.Int
+type Number struct {
+	big.Int
+}
 
 func (*Number) value() {}
 
-type List []Value
+type List struct {
+	data []namedValue
+}
 
 func (List) value() {}
 
 type Block struct {
-	fromGo  func(*stack, []namedValue) error
-	context map[string]namedValue
-	code    Block
+	env    *environment
+	fromGo func(*environment, []namedValue) (namedValue, error)
+	code   parser.Block
 }
 
 func (*Block) value() {}
 
-type namedValue struct {
-	name  *name
-	value value // nil == no underlying value
-}
+func (b *Block) run(local *environment, args []namedValue) (namedValue, error) {
+	env := local
+	if b.env != nil {
+		env = b.env.merge()
+	}
+	if b.fromGo != nil {
+		return b.fromGo(env, args)
+	}
+	for i, group := range b.code {
+		v, err := evalGroup(env, group)
+		if err != nil {
+			return v, err
+		}
 
-func (v namedValue) onlyName() (string, error) {
-	if v.name == nil || v.value != nil {
-		if v.name == nil {
-			return "", errors.New("value is unnamed")
+		if i == len(b.code)-1 {
+			return v, nil
 		} else {
-			return "", fmt.Errorf("%s has underlying value %#v", v.name.n, v.value)
+			if _, ok := v.value.(Unit); !ok {
+				// TODO: better error message with possible name
+				return v, fmt.Errorf("non unit value %#v in not last group of block", v.value)
+			}
 		}
 	}
-	return v.name.n, nil
+	return namedValue{nil, Unit{}}, nil
 }
 
-type name struct {
-	n    string
-	next *name
-}
+func evalGroup(env *environment, group parser.Group) (namedValue, error) {
+	switch len(group) {
+	case 0:
+		return namedValue{nil, Unit{}}, nil
+	case 1:
+		return evalElement(env, group[0])
+	case 3:
+		rhs, ok0 := group[0].(parser.Group)
+		symbol, ok1 := group[1].(parser.Symbol)
+		lhs, ok2 := group[2].(parser.Group)
 
-// lookup var
-// copy environment
+		if ok0 && ok1 && ok2 {
+			rhsValue, err := evalGroup(env, rhs)
+			if err != nil {
+				return rhsValue, err
+			}
+			lhsValue, err := evalGroup(env, lhs)
+			if err != nil {
+				return lhsValue, err
+			}
 
-type stack struct {
-	m map[string]namedValue
-	s [][]string
-}
+			symbolValue, ok := env.get(string(symbol))
+			if !ok {
+				return namedValue{}, fmt.Errorf("unknown operator %s", symbol)
+			}
+			block, ok := symbolValue.value.(*Block)
+			if !ok {
+				return symbolValue, fmt.Errorf(
+					"operator %s is not a block, but a %#v value instead",
+					symbol,
+					symbolValue,
+				)
+			}
+			return block.run(env, []namedValue{rhsValue, lhsValue})
+		}
+		fallthrough
+	default:
+		name, ok := group[0].(parser.Atom)
+		if !ok {
+			// TODO: parser: check atom is first
+			panic(internal)
+		}
 
-func newStack() *stack {
-	return &stack{
-		m: make(map[string]namedValue),
-		s: nil,
+		args := make([]namedValue, len(group)-1)
+		for i, e := range group[1:] {
+			v, err := evalElement(env, e)
+			if err != nil {
+				return v, err
+			}
+			args[i] = v
+		}
+
+		nameValue, ok := env.get(string(name))
+		if !ok {
+			return namedValue{}, fmt.Errorf("name %s not found", name)
+		}
+		block, ok := nameValue.value.(*Block)
+		if !ok {
+			return nameValue, fmt.Errorf(
+				"name %s is not a block, but a %#v value instead",
+				name,
+				nameValue,
+			)
+		}
+		return block.run(env, args)
 	}
 }
 
-func (s *stack) push() {
-	s.s = append(s.s, nil)
-}
-
-func (s *stack) pop() {
-	for _, n := range s.s[len(s.s)-1] {
-		delete(s.m, n)
-	}
-	s.s = s.s[:len(s.s)-1]
-	if len(s.s) == 0 {
+func evalElement(env *environment, element parser.Element) (namedValue, error) {
+	switch v := element.(type) {
+	case parser.Atom:
+		val, _ := env.get(string(v))
+		return namedValue{&name{string(v), val.name}, val.value}, nil
+	case parser.String:
+		return namedValue{nil, String(v)}, nil
+	case *parser.Number:
+		n := Number(*v)
+		return namedValue{nil, &n}, nil
+	case parser.List:
+		l := make([]namedValue, len(v))
+		for i, e := range v {
+			v, err := evalElement(env, e)
+			if err != nil {
+				return v, err
+			}
+			l[i] = v
+		}
+		return namedValue{nil, List{l}}, nil
+	case parser.Group:
+		return evalGroup(env, v)
+	case parser.Block:
+		return namedValue{nil, &Block{env: env.merge(), code: v}}, nil
+	default:
 		panic(internal)
 	}
 }
 
-func (s *stack) get(name string) (namedValue, bool) {
-	v, ok := s.m[name]
+// TODO: use persistent maps
+type environment struct {
+	outer map[string]namedValue
+	local map[string]namedValue
+}
+
+func (e *environment) get(name string) (namedValue, bool) {
+	v, ok := e.local[name]
+	if !ok {
+		v, ok = e.outer[name]
+	}
 	return v, ok
 }
 
-func (s *stack) put(name string, v namedValue) bool {
-	if _, ok := s.m[name]; ok {
+func (e *environment) put(name string, v namedValue) bool {
+	_, ok0 := e.local[name]
+	_, ok1 := e.outer[name]
+	if ok0 || ok1 {
 		return false
 	}
-	s.m[name] = v
-	s.s[len(s.s)-1] = append(s.s[len(s.s)-1], name)
+	if e.local == nil {
+		e.local = make(map[string]namedValue)
+	}
+	e.local[name] = v
 	return true
+}
+
+func (e *environment) merge() *environment {
+	outer := e.outer
+	if len(e.local) > 0 {
+		outer = make(map[string]namedValue, len(e.outer)+len(e.local))
+		for k, v := range e.outer {
+			outer[k] = v
+		}
+		for k, v := range e.local {
+			outer[k] = v
+		}
+	}
+	return &environment{outer: outer}
+}
+
+var builtins = []struct {
+	name string
+	fn   func(*environment, []namedValue) (namedValue, error)
+}{
+	{"=", func(env *environment, args []namedValue) (namedValue, error) {
+		if len(args) != 2 {
+			panic(internal) // op can only be called with 2 args
+		}
+		assignee, err := args[0].onlyName()
+		if err != nil {
+			return namedValue{}, fmt.Errorf("can't assign to name, %w", err)
+		}
+		if ok := env.put(assignee, args[1]); !ok {
+			return namedValue{}, fmt.Errorf("couldn't assign to name, %s already exists", assignee)
+		}
+		return namedValue{nil, Unit{}}, nil
+	}},
+	{"+", func(_ *environment, args []namedValue) (namedValue, error) {
+		if len(args) != 2 {
+			panic(internal) // op can only be called with 2 args
+		}
+		xValue, err := args[0].get()
+		if err != nil {
+			return namedValue{}, fmt.Errorf("can't add, %w", err)
+		}
+		yValue, err := args[1].get()
+		if err != nil {
+			return namedValue{}, fmt.Errorf("can't add, %w", err)
+		}
+		x, ok := xValue.(*Number)
+		if !ok {
+			return args[0], fmt.Errorf("can't add, %#v is not a number", args[0])
+		}
+		y, ok := yValue.(*Number)
+		if !ok {
+			return args[1], fmt.Errorf("can't add, %#v is not a number", args[1])
+		}
+		var b big.Int
+		return namedValue{nil, &Number{*b.Add(&x.Int, &y.Int)}}, nil
+	}},
+	{"println", func(_ *environment, args []namedValue) (namedValue, error) {
+		for i, v := range args {
+			_, err := fmt.Print(v.value)
+			if err != nil {
+				return namedValue{}, err
+			}
+			if i < len(args)-1 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+		return namedValue{nil, Unit{}}, nil
+	}},
 }
 
 // when adding blocks, all unknown vars are marked and attached to the block definition.
@@ -128,63 +318,13 @@ func (s *stack) put(name string, v namedValue) bool {
 //	evaluated (arguments treated like this as well) and stored as value:
 //		(Atom ...) (Group Symbol Group)
 func Run(block parser.Block) error {
-	i := &interpreter{
-		s: newStack(),
-	}
-
-	builtins := []struct {
-		name string
-		fn   func(args []namedValue) error
-	}{
-		{"=", func(args []namedValue) error {
-			if len(args) != 2 {
-				panic(internal) // op can only be called with 2 args
-			}
-			assignee, err := args[0].onlyName()
-			if err != nil {
-				return fmt.Errorf("can't assign to name, %w", err)
-			}
-			if ok := i.s.put(assignee, args[1]); !ok {
-				return fmt.Errorf("couldn't assign to name, %s already exists", assignee)
-			}
-			return nil
-		}},
-	}
+	b := Block{env: new(environment), code: block}
 	for _, builtin := range builtins {
-		v := namedValue{nil, Block{fromGo: builtin.fn}}
-		if ok := i.s.put(builtin.name, v); !ok {
+		v := namedValue{nil, &Block{fromGo: builtin.fn}}
+		if ok := b.env.put(builtin.name, v); !ok {
 			panic(internal)
 		}
 	}
-
-	return r.run(block)
-}
-
-type interpreter struct {
-	s *stack
-}
-
-func (i *interpreter) run(block parser.Block, args []namedValue) error {
-	i.s.push()
-	defer i.s.pop()
-
-	for _, group := range block {
-		switch len(group) {
-		case 0:
-			return Unit, nil
-		case 1:
-		case 3:
-			_, ok0 := group[0].(parser.Group)
-			_, ok1 := group[1].(parser.Symbol)
-			_, ok2 := group[2].(parser.Group)
-			if !ok0 || !ok1 || !ok2 {
-				fallthrough
-			}
-		default:
-			name, ok := group[0].(parser.Atom)
-			if !ok {
-				panic("TODO: parser: check atom is first")
-			}
-		}
-	}
+	_, err := b.run(nil, nil)
+	return err
 }
