9896d89dec030d9c3948632b9ed7b06992d19367
Erik Fastermann <erik@fastermann.de>
2021-08-06 14:55:47 +0200

    lexer refactor

--- parser/lexer.go
+++ parser/lexer.go
@@ -69,6 +69,8 @@ func (l *Lexer) Next() (Token, error) {
 	return t, nil
 }
 
+var errBareTick = errors.New("bare '")
+
 func (l *Lexer) next() (Token, error) {
 	ch, _, err := l.r.ReadRune()
 	if err != nil {
@@ -80,167 +82,167 @@ func (l *Lexer) next() (Token, error) {
 			return EndOfLine{}, nil
 		}
 		for {
-			chch, _, err := l.r.ReadRune()
+			ch, _, err = l.r.ReadRune()
 			if err != nil {
 				return nil, err
 			}
-			if !unicode.IsSpace(chch) {
-				ch = chch
+			if !unicode.IsSpace(ch) {
 				break
-			}
-			if chch == '\n' {
-				return EndOfLine{}, nil
-			}
-		}
-	}
-
-	switch ch {
-	case '"':
-		var str strings.Builder
-		for {
-			ch, _, err := l.r.ReadRune()
-			if err != nil {
-				if err == io.EOF {
-					return nil, errors.New("string not closed with \"")
-				}
-				return nil, err
-			}
-			if ch == '"' {
-				break
-			} else if ch == '\r' {
-			} else {
-				str.WriteRune(ch)
-			}
-		}
-		return String(str.String()), nil
-	case '(':
-		return OpenBracket{}, nil
-	case ')':
-		return ClosedBracket{}, nil
-	case '[':
-		return OpenSquare{}, nil
-	case ']':
-		return ClosedSquare{}, nil
-	case '{':
-		return OpenCurly{}, nil
-	case '}':
-		return ClosedCurly{}, nil
-	case '#':
-		for {
-			ch, _, err := l.r.ReadRune()
-			if err != nil {
-				if err == io.EOF {
-					// assumed that subsequent reads will return io.EOF
-					return EndOfLine{}, nil
-				}
-				return nil, err
 			}
 			if ch == '\n' {
 				return EndOfLine{}, nil
 			}
 		}
-	case '\'':
-		// TODO: check first char is not a number
+	}
 
-		var atom strings.Builder
-		for {
+	switch {
+	case isReservedSymbol(ch):
+		switch ch {
+		case '"':
+			var str strings.Builder
+			for {
+				ch, _, err := l.r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						return nil, errors.New("string not closed with \"")
+					}
+					return nil, err
+				}
+				if ch == '"' {
+					break
+				} else if ch == '\r' {
+				} else {
+					str.WriteRune(ch)
+				}
+			}
+			return String(str.String()), nil
+		case '\'':
 			ch, _, err := l.r.ReadRune()
 			if err != nil {
 				if err == io.EOF {
-					break
+					return nil, errBareTick
 				}
 				return nil, err
 			}
-			if !isAtomChar(ch) {
-				must(l.r.UnreadRune())
-				break
+			if !isCharStart(ch) {
+				return nil, errBareTick
 			}
-			atom.WriteRune(ch)
-		}
-		return Atom(atom.String()), nil
-	default:
-		if ch >= '0' && ch <= '9' {
-			// TODO: support hex, binary, octal
-			// TODO: support .
+			must(l.r.UnreadRune())
 
-			var num strings.Builder
-			num.WriteRune(ch)
-			for {
-				ch, _, err := l.r.ReadRune()
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					return nil, err
-				}
-				if (ch < '0' || ch > '9') && ch != '_' {
-					must(l.r.UnreadRune())
-					break
-				}
-				num.WriteRune(ch)
-			}
-
-			numStr := num.String()
-			if len(numStr) > 1 && numStr[0] == '0' {
-				return nil, fmt.Errorf("zero padded number %q", numStr)
-			}
-
-			n, err := number.FromString(numStr)
+			atom, err := l.takeStringWhile(isChar)
 			if err != nil {
-				panic(internal + ": " + err.Error())
+				return nil, err
 			}
-			return Number(n), nil
-		} else if isAtomChar(ch) {
-			var ref strings.Builder
-			ref.WriteRune(ch)
+			return Atom(atom), nil
+		case '(':
+			return OpenBracket{}, nil
+		case ')':
+			return ClosedBracket{}, nil
+		case '[':
+			return OpenSquare{}, nil
+		case ']':
+			return ClosedSquare{}, nil
+		case '{':
+			return OpenCurly{}, nil
+		case '}':
+			return ClosedCurly{}, nil
+		case '#':
 			for {
 				ch, _, err := l.r.ReadRune()
 				if err != nil {
 					if err == io.EOF {
-						break
+						return EndOfLine{}, nil
 					}
 					return nil, err
 				}
-				if !isAtomChar(ch) {
-					must(l.r.UnreadRune())
-					break
+				if ch == '\n' {
+					return EndOfLine{}, nil
 				}
-				ref.WriteRune(ch)
 			}
-			return Ref(ref.String()), nil
-		} else if isSymbol(ch) {
-			var symbol strings.Builder
-			symbol.WriteRune(ch)
-			for {
-				ch, _, err := l.r.ReadRune()
-				if err != nil {
-					if err == io.EOF {
-						break
-					}
-					return nil, err
-				}
-				if !isSymbol(ch) {
-					must(l.r.UnreadRune())
-					break
-				}
-				symbol.WriteRune(ch)
-			}
-			return Symbol(symbol.String()), nil
-		} else {
-			return nil, fmt.Errorf("unknown character %q", ch)
+		default:
+			panic(internal)
 		}
+	case isCharStart(ch):
+		must(l.r.UnreadRune())
+		ref, err := l.takeStringWhile(isChar)
+		if err != nil {
+			return nil, err
+		}
+		return Ref(ref), nil
+	case isNumberStart(ch):
+		// TODO: support hex, binary, octal
+		// TODO: support .
+
+		must(l.r.UnreadRune())
+		num, err := l.takeStringWhile(isNumber)
+		if err != nil {
+			return nil, err
+		}
+		if len(num) > 1 && num[0] == '0' {
+			return nil, fmt.Errorf("zero padded number %q", num)
+		}
+
+		n, err := number.FromString(num)
+		if err != nil {
+			panic(internal + ": " + err.Error())
+		}
+		return Number(n), nil
+	case isSymbol(ch):
+		must(l.r.UnreadRune())
+		symbol, err := l.takeStringWhile(isSymbol)
+		if err != nil {
+			return nil, err
+		}
+		return Symbol(symbol), nil
+	default:
+		return nil, fmt.Errorf("unknown character %q", ch)
 	}
 }
 
-func isAtomChar(ch rune) bool {
-	return unicode.IsLetter(ch) || unicode.IsNumber(ch) || ch == '_'
+func (l *Lexer) takeStringWhile(predicate func(rune) bool) (string, error) {
+	var b strings.Builder
+	for {
+		ch, _, err := l.r.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		if !predicate(ch) {
+			must(l.r.UnreadRune())
+			break
+		}
+		b.WriteRune(ch)
+	}
+	return b.String(), nil
+}
+
+func isCharStart(ch rune) bool {
+	return !isNumberStart(ch) && (unicode.IsLetter(ch) || ch == '_')
+}
+
+func isChar(ch rune) bool {
+	return unicode.IsLetter(ch) || isNumberStart(ch) || ch == '_'
+}
+
+func isReservedSymbol(ch rune) bool {
+	switch ch {
+	case '"', '\'', '(', ')', '{', '}', '[', ']', '#':
+		return true
+	default:
+		return false
+	}
 }
 
 func isSymbol(ch rune) bool {
-	switch ch {
-	case '"', '\'', '(', ')', '{', '}', '[', ']', '_':
-		return false
-	default:
-		return unicode.IsSymbol(ch) || unicode.IsPunct(ch)
-	}
+	return !isReservedSymbol(ch) && ch != '_' && (unicode.IsSymbol(ch) || unicode.IsPunct(ch))
+}
+
+func isNumberStart(ch rune) bool {
+	return ch >= '0' && ch <= '9'
+}
+
+func isNumber(ch rune) bool {
+	return (ch >= '0' && ch <= '9') || ch == '_'
 }
